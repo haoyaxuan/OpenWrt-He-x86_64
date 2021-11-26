74416bb8a23f499f64fb6e307c7b9fbe7cef58b5
haoyaxuan <haoyaxuan@aliyun.com>
2021-11-26 10:19:21 +0800

    精简

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@ sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt-Eleven '/g
 sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-He'/g" ./package/base-files/files/bin/config_generate
 # curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/poweroff.htm > ./feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_system/poweroff.htm 
 # curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
-curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/logo.jpg > .package/luci-app-serverchan/root/usr/bin/serverchan/api/logo.jpg
+curl -fsSL  https://raw.githubusercontent.com/haoyaxuan/OpenWrt-He-x86_64/main/images/chan.png > .package/luci-app-serverchan/root/usr/bin/serverchan/api/logo.jpg
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 rm -rf ./package/lean/luci-app-qbittorrent
